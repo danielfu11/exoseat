95e71eec7ed8f9e32c4a5ca0883a1cfc4a3e2f40
Sonya11 <sonya.schuppan@colorado.edu>
2020-02-26 15:57:06 -0700

    source and include files only

---
+++ Final/inc/speed_control.h
@@ -0,0 +1,19 @@
+/*
+ * speed_control.h
+ *
+ *  Created on: Jan 27, 2020
+ *      Author: sonya
+ */
+
+#ifndef SPEED_CONTROL_H_
+#define SPEED_CONTROL_H_
+
+#include "DCL.h"
+#include "DCLF32.h" //32 bit floating point implementation
+
+void controller_init(void);
+
+
+
+
+#endif /* SPEED_CONTROL_H_ */
